608a9922af736b5125034897168ea4657f1cf846
Eric Aggrey <ericaggrey@outlook.com>
2022-07-01 18:39:01 +0200

    added firebase and saved todo locally

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,44 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const  [todos, setTodos] = useState([
+  'create a git repository',
+  'created  a react vite project'
+])
+const [input , setInput] = useState("")
+
+const addTodo = function(e) {
+  e.preventDefault()
+  setTodos([...todos, input])
+  setInput("")
+  
+}
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div >
+      <h1>MCI TODO APP</h1>
+      <form>
+        <br/>
+  
+      <input  type='text' value={input} onChange={(e)=>setInput(e.target.value)} />
+      <button  type="submit"
+      onClick={addTodo}
+      >Add Todo</button>
+
+
+      </form>
+
+      <ul>
+       
+       {todos.map(function(todo) {
+          return(
+            <li>{todo}</li>
+          )
+       })}
+
+
+      </ul>
     </div>
   )
 }
---
+++ src/Firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnUtqykVSiazYjGG0e8Idnlc7qoxhSgBQ",
+  authDomain: "mcitodo-623cc.firebaseapp.com",
+  projectId: "mcitodo-623cc",
+  storageBucket: "mcitodo-623cc.appspot.com",
+  messagingSenderId: "629074297160",
+  appId: "1:629074297160:web:ee46c277875e0cc419737d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
