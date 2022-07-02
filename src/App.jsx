81ec493293ad053cffb51ec862dfd40a45d8acb9
Eric Aggrey <ericaggrey@outlook.com>
2022-07-02 13:50:11 +0200

    added firebase hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,88 @@
+import React from 'react';
 import { useState, useEffect } from 'react'
-import {db} from './Firebase'
+import { getFirestore } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { app } from './Firebase'
+import { collection, getDocs, addDoc , doc, deleteDoc} from "firebase/firestore";
+
+
+
+
+
 
 import './App.css'
 
 function App() {
-const  [todos, setTodos] = useState([])
-const [input , setInput] = useState("")
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+
+  const [value, loading, error] = useCollection(collection(getFirestore(app), 'todos'),);
+
+  const addTodo = function (e) {
+    e.preventDefault()
+    addDoc(collection(getFirestore(app), "todos"), {todo:input}).then((d)=>{
+      console.log("Document written with ID: ", d.id);
+    }).catch((e)=>{
+      console.error("Error adding document: ", e);
+    })
+    setInput("")
+
+  }
 
 
-useEffect(()=>{
-  db.collection('todos').onSnapshot(
-    snapshot=>{
-      setTodos(snapshot.docs.map(doc=>doc.data))
-    }
-  )
-},[input])
-
-
-const addTodo = function(e) {
-  e.preventDefault()
-  setTodos([...todos, input])
-  setInput("")
+  const deleteTodo = (id)=>{
+    deleteDoc(doc(getFirestore(app), "todos", id)).then((d)=>{
+      console.log("deleted")
+    }).catch((e)=>console.log(e))
   
-}
+  }
+  
 
   return (
-    <div >
-      <h1>MCI TODO APP</h1>
-      <form>
-        <br/>
-  
-      <input  type='text' value={input} onChange={(e)=>setInput(e.target.value)} />
-      <button  type="submit"
-      onClick={addTodo}
-      >Add Todo</button>
+
+    <div>
+      <p>
+        {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Collection: Loading...</span>}
+        {value && (
+          <>
+            <div >
+              <h1>MCI TODO APP</h1>
+              <form>
+                <br />
+
+                <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+                <button type="submit"
+                  onClick={addTodo}
+                >Add Todo</button>
 
 
-      </form>
-
-      <ul>
-       
-       {todos.map(function(todo) {
-          return(
-            <li>{todo}</li>
-          )
-       })}
+              </form>
 
 
-      </ul>
+
+            </div>
+
+            <span>
+           
+              {value.docs.map((doc) => (
+                <React.Fragment key={doc.id}>
+    
+                <span>{doc.data()['todo']} <button>X</button></span> 
+                <br/>
+                </React.Fragment>
+               
+              ))}
+            </span>
+
+          </>
+        )}
+      </p>
     </div>
+
+
+
+
   )
 }
 
